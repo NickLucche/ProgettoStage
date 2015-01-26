a10df7b7f0a88a2fc5d52d77327625fc4355bc85
NickLucche <nicolo.lucchesi@gmail.com>
2015-01-26 16:29:12 +0100

    Aggiunto progetto mini Chat

---
+++ ChatMessenger/src/Server.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.net.*;
+public class Server {
+
+	public Server()throws IOException{
+		ServerSocket serverSock = new ServerSocket(6789);
+		
+		Socket sock = serverSock.accept();
+		System.out.println("Client connesso!");
+		BufferedReader bfKeyboard = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader bfInput = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
+		String toSend;
+		String received;
+		while(true){
+			toSend = bfKeyboard.readLine();
+			out.write(toSend+"\n");
+			out.flush();
+			System.out.println("Server> "+toSend);
+			received = bfInput.readLine();
+			System.out.println("Client> "+received);
+		}
+		
+		
+		
+	}
+	
+	public static void main(String args[]) throws IOException{
+		Server s = new Server();
+	}
+}
